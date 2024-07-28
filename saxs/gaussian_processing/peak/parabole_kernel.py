6412a991da90356f5fce64773aea2ebd660901dd
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2024-07-28 10:39:16 +0200

    more accurate minimize method replaces curve_fit for parabole (but seems not good)

--- saxs/gaussian_processing/peak/parabole_kernel.py
+++ saxs/gaussian_processing/peak/parabole_kernel.py
@@ -34,7 +34,50 @@ class ParabolePeakKernel(ProminencePeakKernel):
     def negative_reduction(self):
         self.current_I_state = np.maximum(self.current_I_state, 0)
 
-    def parabole_peak_fitting(self, i):
+    def parabole_peak_minimize(self, i):
+        if len(self.peaks) > i:
+            if np.size(self.peaks) != 0:
+                # left_base = abs(self.peaks[i] - self.props['left_bases'][i])
+                # right_base = abs(self.peaks[i] - self.props['right_bases'][i])
+
+                # delta = min(left_base, right_base) / 2
+
+                delta = 4
+                start_delta = delta
+
+                period1 = int(self.peaks[i] - delta)
+                period2 = int(self.peaks[i] + delta)
+
+                def loss_function(params):
+                    sigma = params[0]
+                    ampl = params[1]
+                    y_pred = parabole(self.current_q_state, self.current_q_state[self.peaks[i]], sigma, ampl)
+                    # return np.sum((y_pred - self.I_background_filtered) ** 2)
+                    return np.sum((y_pred[period1:period2] - self.I_background_filtered[period1:period2]) ** 2)
+
+                result = minimize(loss_function, np.array([0.5, 2]))
+                fitted_params = result.x
+
+
+                period2 = int(self.peaks[i] + start_delta)
+                period1 = int(self.peaks[i] - start_delta)
+
+                current_parabole = parabole(self.current_q_state, self.current_q_state[self.peaks[i]], fitted_params[0], fitted_params[1])
+
+                plt.clf()
+                plt.plot(self.current_q_state, self.current_I_state)
+                # plt.plot(self.current_q_state[period1//2:period2*2], current_parabole[period1//2:period2*2])
+                plt.plot(self.current_q_state[period1:period2], self.current_I_state[period1:period2])
+                plt.plot(self.current_q_state[period1:period2], current_parabole[period1:period2], 'red')
+
+                plt.title(f'{fitted_params}')
+                plt.savefig('{}parabole_{}.png'.format(self.file_analysis_dir_peaks, self.parabole_number))
+
+                self.parabole_number += 1
+                self.parabole_popt_for_gauss[self.peaks[i]] = fitted_params
+
+
+    def parabole_peak_curve_fit(self, i):
         if len(self.peaks) > i:
             if np.size(self.peaks) != 0:
                 # left_base = abs(self.peaks[i] - self.props['left_bases'][i])
@@ -183,7 +226,8 @@ class ParabolePeakKernel(ProminencePeakKernel):
             if len(self.peaks) == 0:
                 break
 
-            self.parabole_peak_fitting(peak_counter)
+            self.parabole_peak_curve_fit(peak_counter)
+            # self.parabole_peak_minimize(peak_counter)
             self.gauss_peak_fitting_minimize(peak_counter)
             # self.gauss_peak_fitting_curve_fit(peak_counter)
             self.gaussian_peak_reduction(peak_counter)
